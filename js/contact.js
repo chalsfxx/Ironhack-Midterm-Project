cee3eccc2f3e9cdca21c83d5fd02e306133a0c50
chalsfxx <e.ferrerllc@gmail.com>
2022-11-25 12:09:13 +0100

    hola

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,53 @@
-const sendForm = (preventForm) => {
-  preventForm.preventDefault();
+// const sendForm = (preventForm) => {
+//   preventForm.preventDefault();
 
-  let name = document.querySelector("#name").value;
+//   let name = document.querySelector("#name").value;
 
-  let email = document.querySelector("#email").value;
+//   let email = document.querySelector("#email").value;
 
-  let phone = document.querySelector("#phone").value;
+//   let phone = document.querySelector("#phone").value;
 
-  let message = document.querySelector("#message").value;
+//   let message = document.querySelector("#message").value;
 
 
 
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
+//   fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
 
-    body: JSON.stringify({
-      title: name,
+//     body: JSON.stringify({
+//       title: name,
 
-      body: `${email} ${phone} ${message}`,
+//       body: `${email} ${phone} ${message}`,
 
-      userId: 1,
-    }),
+//       userId: 1,
+//     }),
 
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => {
-      if (response.status === 201) {
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((response) => {
+//       if (response.status === 201) {
 
-        document.querySelector(".alert").style.display = "block";
+//         document.querySelector(".alert").style.display = "block";
 
-        setTimeout(
-          () => (document.querySelector(".alert").style.display = "none"),
-          6000
-        );
-      } else {
-        document.querySelector(".alert-wrong").style.display = "block";
+//         setTimeout(
+//           () => (document.querySelector(".alert").style.display = "none"),
+//           6000
+//         );
+//       } else {
+//         document.querySelector(".alert-wrong").style.display = "block";
 
-        setTimeout(
-          () => (document.querySelector(".alert-wrong").style.display = "none"),
-          6000
-        );
-      }
+//         setTimeout(
+//           () => (document.querySelector(".alert-wrong").style.display = "none"),
+//           6000
+//         );
+//       }
 
-      return response.json();
-    })
+//       return response.json();
+//     })
 
-    .then((json) => console.log(json));
-};
+//     .then((json) => console.log(json));
+// };
 
-document.querySelector("#contact-form").addEventListener("submit", sendForm);
+// document.querySelector("#contact-form").addEventListener("submit", sendForm);
--- js/index.js
+++ js/index.js
@@ -1,33 +1,41 @@
 
-
-const get3posts = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-
-    .then((res) => {
-      for (let i = 0; i < 3; i++) {
-        document.querySelector(
-          ".projects-container"
-        ).innerHTML += `<div class="project">
+console.log("hola");
 
 
-          <img src="./${res[i].id}.jpg" alt="">
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'Simplify',
+    body: '  Simplify is lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat faucibus sem, id vulputate justo ornare non. Nullam et vehicula sem. Pellentesque bibendum massa non nisl posuere cursus. Aliquam ac eros pretium neque consequat facilisis. Ut sit amet justo varius ex ultrices fringilla. Sed vel turpis fringilla ante gravida vestibulum. Etiam a sollicitudin leo. Nunc metus lectus, lacinia ut arcu quis, pretium congue lacus,Aliquam blandit varius metus, in mattis orci convallis sit amet.',
+    userId: 'Fusce et sem ac velit placerat ultrices in id dolor. Sed eget auctor metus. Sed suscipit fringilla lectus, a maximus justo bibendum a. Donec et pellentesque nisl, vitae tempus lorem. Sed efficitur cursus rhoncus. Duis placerat nisi quis mi tincidunt, sit amet tincidunt urna convallis. Nam et sagittis felis. Pellentesque placerat orci sodales nisi convallis ultrices. Nunc posuere neque eu mi imperdiet ullamcorper. Praesent tempor convallis rutrum. Vestibulum dignissim pellentesque eros in placerat. Etiam at nisi quis quam sollicitudin vehicula sit amet vel mi',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => {
+    document.querySelector('#title').innerHTML = json.title
+    document.querySelector('#paragraph-one').innerHTML = json.body
+    document.querySelector('#paragraph-two').innerHTML = json.userId
+    console.log(json);
+  })
+  .catch((error) => console.log("error"));
+   
 
+document.querySelector('#form').onSubmit = (e) => {
+  e.preventDefault();
+   let name = document.querySelector("#name").value;
 
-          <h3>${res[i].title}</h3>
+  let email = document.querySelector("#email").value;
 
+ let phone = document.querySelector("#phone").value;
 
-          <p>${res[i].body}</p>
+  let message = document.querySelector("#message").value;
+  if (name === "" || message === "") {
+    alert("Make sure the form is completed")
+    
+  } else (console.log('${name} registrado'));
+}
 
-
-          <a href="project.html?p=${res[i].id}">Learn More</a>
-
-
-          </div>`;
-      }
-    })
-
-    .catch((error) => console.log(error));
-};
-
-window.addEventListener("load", get3posts);
+    
